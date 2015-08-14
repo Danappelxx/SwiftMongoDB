5c7abe83753b9383f20a96b2d4938c7188a5b159
Dan Appel <Dan.appel00@gmail.com>
2015-08-14 11:07:29 -0700

    Initial Commit

---
+++ swiftMongoDB/swiftMongoDB.h
@@ -0,0 +1,19 @@
+//
+//  swiftMongoDB.h
+//  swiftMongoDB
+//
+//  Created by Dan Appel on 8/14/15.
+//  Copyright © 2015 Dan Appel. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for swiftMongoDB.
+FOUNDATION_EXPORT double swiftMongoDBVersionNumber;
+
+//! Project version string for swiftMongoDB.
+FOUNDATION_EXPORT const unsigned char swiftMongoDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swiftMongoDB/PublicHeader.h>
+
+
