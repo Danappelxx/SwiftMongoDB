b971f03420891cf6261bcaebf8896f518b58e2f6
Dan Appel <Dan.appel00@gmail.com>
2015-08-28 21:57:36 -0700

    implement login function (not tested)

--- swiftMongoDB/MongoDB.swift
+++ swiftMongoDB/MongoDB.swift
@@ -14,7 +14,18 @@ public class MongoDB {
     
     internal var connection: UnsafeMutablePointer<mongo>? = mongo_alloc()
 
-    public var databaseName: String
+    internal var databaseName: String
+    
+    public var db: String { // read only
+        get {
+            return self.databaseName
+        }
+    }
+    
+    public func changeDB(databaseName: String) {
+        self.databaseName = databaseName
+    }
+
     internal var collections = Set<MongoCollection>()
     
 
@@ -26,13 +37,12 @@ public class MongoDB {
     - parameter database:    The database, ex: 'test'
     - parameter userAndPass: The username and password for the user in the format (username, password) - optional.
     */
-    public init(host: String, port: Int, database: String, usernameAndPassword userAndPass:(String,String)? = nil) {
+    public init(host: String, port: Int, database: String, usernameAndPassword userAndPass:(username: String, password: String)? = nil) {
         
         mongo_init(self.connection!)
 
-
         let status = mongo_client(self.connection!, host, Int32(port))
-        
+
         if status != MONGO_OK {
             print("connection failed")
             mongo_destroy(self.connection!)
@@ -44,19 +54,26 @@ public class MongoDB {
 
 
         // implement this later
-//        if let userAndPass = userAndPass {
-//            
-//            mongo_cmd_authenticate(self.connection!, self.databaseName, userAndPass.0, userAndPass.1)
-//        }
+        if let userAndPass = userAndPass {
+            self.login(username: userAndPass.username, password: userAndPass.password)
+        }
+    }
+    
+    public func login(username username: String, password: String) {
+        mongo_cmd_authenticate(self.connection!, self.db, username, password)
     }
     
     /**
     Initates the mongodb connection to the default host and port (usually localhost:27017)
     */
-    public convenience init(database: String, usernameAndPassword userAndPass:(String,String)? = nil) {
+    public convenience init(database: String, usernameAndPassword userAndPass:(username: String, password: String)? = nil) {
         
         self.init(host: Helpers.getIPAddress(), port: 27017, database: database, usernameAndPassword: userAndPass)
-        
+
+
+        if let userAndPass = userAndPass {
+            self.login(username: userAndPass.username, password: userAndPass.password)
+        }
     }
     
     /**
@@ -117,5 +134,13 @@ public class MongoDB {
         collection.databaseName = self.databaseName
 
         self.collections.insert(collection)
+    }
+
+    public func createUser(user user: String, password: String) -> Bool{
+
+        let addUserResult = mongo_cmd_add_user(self.connection!, self.db, user, password)
+        print(addUserResult)
+        
+        return (addUserResult == MONGO_OK)
     }
 }
