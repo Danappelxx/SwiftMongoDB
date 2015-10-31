218984a23b5c1c5a9276d7d6fabfbc7c64ce4361
Dan Appel <dan.appel00@gmail.com>
2015-10-31 15:13:34 -0700

    Create scheme for iOS

--- SwiftMongoDB/SwiftMongoDB.h
+++ SwiftMongoDB/SwiftMongoDB.h
@@ -1,20 +1,20 @@
 //
-//  swiftMongoDB.h
-//  swiftMongoDB
+//  SwiftMongoDB.h
+//  SwiftMongoDB
 //
 //  Created by Dan Appel on 8/14/15.
 //  Copyright © 2015 Dan Appel. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-
 #import "mongoc.h"
 
-//! Project version number for swiftMongoDB.
-FOUNDATION_EXPORT double swiftMongoDBVersionNumber;
+//! Project version number for SwiftMongoDB.
+FOUNDATION_EXPORT double SwiftMongoDBVersionNumber;
 
-//! Project version string for swiftMongoDB.
-FOUNDATION_EXPORT const unsigned char swiftMongoDBVersionString[];
+//! Project version string for SwiftMongoDB.
+FOUNDATION_EXPORT const unsigned char SwiftMongoDBVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <swiftMongoDB/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMongoDB/PublicHeader.h>
+
 
